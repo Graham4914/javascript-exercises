ce890d9e7a5b3b2693487d5de1d1b3426025a7ce
Graham S <graham@sharmandigital.com>
2023-10-06 09:29:53 +0100

    exercises completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+const finalString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+return finalString.split("").reverse().join("") == finalString;
 };
 
 // Do not edit below this line
